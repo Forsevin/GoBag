170033e5075e294771fdda5e766009d4689368e4
Kevin <kvnforsell@outlook.com>
2014-03-08 20:06:35 +0100

    Create bag.go

---
+++ bag.go
@@ -0,0 +1,70 @@
+package bag
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Bag struct {
+	data []interface{}
+	size int
+}
+
+func NewBag(capacity int) *Bag {
+	return &Bag{
+		data: make([]interface{}, capacity),
+		size: 0,
+	}
+}
+
+// Set index to value
+func (this *Bag) Set(index int, value interface{}) {
+	if index == this.size {
+		this.Grow()
+	}
+
+	this.data[index] = value
+	this.size += 1
+}
+
+// Add value to last avaible element
+func (this *Bag) Add(value interface{}) {
+	this.Set(this.size+1, value)
+}
+
+// Grow by 64
+func (this *Bag) Grow() {
+	oldData := this.data
+	newData := make([]interface{}, this.size+64)
+	copy(oldData, newData)
+	this.data = newData
+}
+
+// Return array (data)
+func (this *Bag) GetData() []interface{} {
+	return this.data
+}
+
+// Get data from index
+func (this *Bag) Get(index int) interface{} {
+	return this.data[index]
+}
+
+// See if bag contains value
+func (this *Bag) Contains(value interface{}) bool {
+	checkval := reflect.TypeOf(value)
+	for _, v := range this.data {
+		if checkval == reflect.TypeOf(v) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Print data
+func (this *Bag) Debug() {
+	for k, v := range this.data {
+		fmt.Println(k, "[", reflect.TypeOf(v), "]")
+	}
+}
